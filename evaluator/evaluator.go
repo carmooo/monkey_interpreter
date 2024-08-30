f6809ec372489c5c6a95608ea64df601d692594d
carmooo <joaorvcarmo@gmail.com>
2024-08-30 11:06:39 +0100

    evaluate expressions
    evaluate expressions starting with integers

---
+++ evaluator/evaluator.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"github.com/carmooo/monkey_interpreter/ast"
+	"github.com/carmooo/monkey_interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	default:
+		return nil
+	}
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range statements {
+		result = Eval(stmt)
+	}
+
+	return result
+}
